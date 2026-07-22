9a3ebe1e149773ee7e9042ce64ee9f0f8cfa891b
Darshan K <darshankulkarni34@gmail.com>
2026-07-22 13:17:48 +0530

    feat: setup controller and routes for healthcheck

---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,9 @@
+import { healthcheck } from "../controllers/healthcheck.controllers.js";
+
+import { Router } from "express";
+
+const router= Router()
+
+router.route("/").get(healthcheck)
+
+export default router
